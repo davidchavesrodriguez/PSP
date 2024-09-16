bfe28c58ec47343a73cdc3ceeca9c63d590c94d7
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-09-16 14:35:47 +0200

    Fixes

---
+++ BasicBirthday/README.md
@@ -0,0 +1,37 @@
+# Basic Birthday
+
+### A22DavidCR
+
+A person wants to reduce the number of candles to be used to celebrate their birthday.\
+To achieve this, the person is going to set their new age in binary.\
+Each candle will represent one bit, and the candle will be lit to represent a 1, or extinguished to indicate a 0.\
+As a result, not only will she use far fewer candles, they won't even have to light them all!\
+The person is so confident that she will finally be able to blow out all the candles that they are even determined to put their age in seconds.
+
+**Input**\
+The program will read, from the standard input, a first number indicating how many test cases will come next.
+
+Each test case is an integer number with the person's age in seconds.
+
+**Output**\
+For each test case the program will write the number of lit candles the person will have to put on his birthday cake, knowing that it represents the number in binary.
+
+**Example input**\
+4
+
+1
+
+2
+
+15
+
+8193
+
+**Example output**\
+1
+
+1
+
+4
+
+2
--- BasicBirthday/src/main/java/org/example/Main.java
+++ BasicBirthday/src/main/java/org/example/Main.java
@@ -1,17 +1,37 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int numberOfAges;
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("How many ages are you telling me? ");
+        numberOfAges = myScanner.nextInt();
+        System.out.println("Ok, now tell me the " + numberOfAges + " ages and I will calculate how many candles you need: ");
+        for (int i = 0; i < numberOfAges; i++) {
+            int ageInput = myScanner.nextInt();
+            String binaryNumber = calculateCandles(ageInput);
+            int candleCounter = 0;
+            for (int j = 0; j < binaryNumber.length(); j++) {
+                if (binaryNumber.charAt(j) == '1') {
+                    candleCounter++;
+                }
+            }
+            if (candleCounter == 0) {
+                System.out.println("You won´t need any candle.");
+            } else if (candleCounter == 1) {
+                System.out.println("You will need 1 candle.");
+            } else {
+                System.out.println("You will need " + candleCounter + " candles.");
+            }
         }
+    }
+
+    public static String calculateCandles(Integer nextAge) {
+        return Integer.toBinaryString(nextAge);
+
     }
 }
