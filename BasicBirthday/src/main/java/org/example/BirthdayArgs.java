6c8e770d33a4656dee36a20f471d4a54da55ce1d
A22DavidCR <A22DavidCR@iessanclemente.net>
2024-09-19 13:42:56 +0200

    Verified BirthdayArgs arguments

--- BasicBirthday/src/main/java/org/example/BirthdayArgs.java
+++ BasicBirthday/src/main/java/org/example/BirthdayArgs.java
@@ -1,18 +1,28 @@
 public class BirthdayArgs {
     public static void main(String[] args) {
-        int numCases = Integer.parseInt(args[0]);
-        int cases = 1;
-        while (cases <= numCases) {
-            int age = Integer.parseInt(args[cases]);
-            String binaryAge = Integer.toBinaryString(age);
-            int counter = 0;
-            for (int i = 0; i < binaryAge.length(); i++) {
-                if (binaryAge.charAt(i) == '1') {
-                    counter++;
+        if (args.length == 0) {
+            System.out.println("Use: BirthdayArgs n age1 age2 ... agen");
+        } else {
+            int numCases = Integer.parseInt(args[0]);
+            if (numCases == args.length - 1) {
+                int cases = 1;
+                while (cases <= numCases) {
+                    int age = Integer.parseInt(args[cases]);
+                    String binaryAge = Integer.toBinaryString(age);
+                    int counter = 0;
+                    for (int i = 0; i < binaryAge.length(); i++) {
+                        if (binaryAge.charAt(i) == '1') {
+                            counter++;
+                        }
+                    }
+                    System.out.println("Case " + cases + ": " + counter);
+                    cases++;
                 }
+            } else {
+                System.out.println("Use: BirthdayArgs n age1 age2 ... agen");
+
             }
-            System.out.println("Case " + cases + ": " + counter);
-            cases++;
         }
+
     }
 }
